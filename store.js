31f38a008f8c73902ee73a92668e70d14c65ceeb
Michael Qassis <mikeqassis@gmail.com>
2019-11-24 19:36:07 -0800

    initial commit

---
+++ store.js
@@ -0,0 +1,54 @@
+const STORE = {
+    questions: [
+        {
+            question: 'How many planets are in our solor system?',
+            answers: [
+                '8',
+                '9',
+                '7',
+                '10'
+            ],
+            answer: '8'
+        },
+        {
+            question: 'What is the largest planet in our solar system?',
+            answers: [
+                'Mercury',
+                'Saturn',
+                'Venus',
+                'Jupiter'
+            ],
+            answer: 'Jupiter'
+        },
+        {
+            question: 'How old is the universe?',
+            answers: [
+                '6.4 trillion years old',
+                '45 billion years old',
+                '13.8 billion years old',
+                '6,000 years old'
+            ],
+            answer: '13.8 billion years old'
+        },
+        {
+            question: 'How fast does light travel?',
+            answers: [
+                '252,051 miles per second',
+                '186,282 miles per second',
+                '423,232 miles per second',
+                '1,000,000 miles per second'
+            ],
+            answer: '186,282 miles per second'
+        },
+        {
+            question: 'What do the variables in the quation "F = M * A" stand for?',
+            answers: [
+                'force, matter, acceleration',
+                'force, mass, acceleration',
+                'fermions, matter, air',
+                'foce, matter, acceleration'
+            ],
+            answer: 'force, mass, acceleration'
+        }       
+    ]
+}
